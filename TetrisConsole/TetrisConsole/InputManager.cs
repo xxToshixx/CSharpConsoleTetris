0247d16ef9fe8e563cc116e9df87362f3a9d064a
xxToshixx <t-ninomiya.1024@hotmail.co.jp>
2013-12-21 18:52:08 +0900

    first commit. just on the way...

---
+++ TetrisConsole/TetrisConsole/InputManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisConsole
+{
+    static class InputManager
+    {
+        public static bool rightKey = false;
+        public static bool leftKey = false;
+        public static bool downkey = false;
+        public static bool spaceKey = false;
+
+        public static int KeyDown()
+        {
+
+            ConsoleKeyInfo c = Console.ReadKey(true);
+            switch (c.Key)
+            {
+                case ConsoleKey.DownArrow:
+                    downkey = true;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    leftKey = true;
+                    break;
+
+                case ConsoleKey.RightArrow:
+                    rightKey = true;
+                    break;
+
+                case ConsoleKey.Spacebar:
+                    spaceKey = true;
+                    break;
+                case ConsoleKey.Escape:
+                    return Program.exitCode;
+                    
+                default :
+                    break;
+            }
+            return 0;
+        }
+
+        public static void clearKey()
+        {
+            rightKey = leftKey = downkey = spaceKey = false;
+        }
+
+
+    }
+}
---
+++ TetrisConsole/TetrisConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisConsole
+{
+    class Program
+    {
+        public static int exitCode = 100;
+        static void Main(string[] args)
+        {
+            Tetris t = new Tetris();
+
+            while(true)
+            {
+                t.Draw();
+                int rtn= InputManager.KeyDown();
+
+                if (rtn != exitCode)
+                {
+                    t.Update();
+                    InputManager.clearKey();
+                }
+                if (rtn == exitCode)
+                    return;
+            }
+        }
+    }
+}
